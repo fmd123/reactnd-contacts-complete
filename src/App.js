e32902779904146299223895344c07b87de3e4a7
fmd123 <fmdavis6@gmail.com>
2018-02-26 06:27:33 -0700

    moving along

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts'
+
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  },
+  {
+    "id": "francesca",
+    "name": "Francesca",
+    "email": "---@gmail.com",
+    "avatarURL": "http://localhost:5001/"
+  },
+]
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts contacts = {contacts} />
       </div>
     )
   }
